570ab9ec6e9611a490a94ce9a2d8784457431ef3
kemchuoihot <thinhpx33@gmail.com>
2025-07-28 17:51:16 +0700

    update

--- Homepage/homepage.js
+++ Homepage/homepage.js
@@ -32,7 +32,7 @@ async function loadProducts() {
     const card = document.createElement("div");
     card.className = "col-sm-6 col-md-4 col-lg-3";
     card.innerHTML = `
-      <a href="./Product/detail.html?id=${data.id}" class="text-decoration-none text-dark">
+      <a href="../Product/detail.html?id=${data.id}" class="text-decoration-none text-dark">
         <div class="card h-100">
           <img src="${data.image}" class="card-img-top" alt="${data.name}" onerror="this.src='default.png'">
           <div class="card-body">
